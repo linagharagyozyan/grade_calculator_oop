50004df2cdca3f8945fa968b50ad0090ba61e910
linagharagyozyan <38084102+linagharagyozyan@users.noreply.github.com>
2018-09-16 18:51:38 +0400

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,48 +1,6 @@
-class Student:
-    def __init__(self, first_name, last_name, student_ID, username, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.student_ID = student_ID
-        self.username = username
-        self.password = password
-
-    def displayUsername(self):
-        self.username = self.first_name + "." + self.last_name
-
-
-    def displayStudent(self):
-        print "First name:", self.first_name ,"\n", "Last name:", self.last_name ,"\n", "ID:", self.student_ID ,"\n", "Username:", self.username
-
-
-student = Student ("Lina", "Gharagyozyan", "A0987645", "student.displayUsername", "")
-student.displayUsername()
-student.displayStudent()
-
-class Grade:
-    min = 0
-    max = 100
-    def __init__(self, grade):
-     self.grade = grade
-
-    def set_grade(self):
-        return 0
-
-    def get_grade(self):
-        print self.grade
-
-    def checkIfValidGrade(self):
-        if self.grade >= 0 and self.grade <= 100:
-            pass
-        else:
-            print "Please, enter a grade in range 0-100"
-
-grade1 = Grade(96)
-Grade.get_grade(grade1)
-Grade.checkIfValidGrade(grade1)
-
 class Assignment:
-    min = 0
-    max = 100
+    __min__ = 0
+    __max__ = 100
     def __init__(self, name, percentage, grade, deadline, status, description):
         self.name = name
         self.percentage = percentage
@@ -60,23 +18,92 @@ class Assignment:
         else:
             print "Please, enter a percentage in range 0-100"
 
-assignment1 = Assignment ("Homework", 15, 96,"2018.15.10", "Submitted", "Find technical specification document for your laptop")
-assignment1.getInfo()
-assignment1.checkIfValidPercentage()
+class User(object):
+    def __init__(self, first_name, last_name, ID, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.ID = ID
+        self.password = password
+
+
+    def displayUsername(self):
+        print self.first_name.lower() + "_" + self.last_name.lower()
+
+
+    def displayInfo(self):
+        print "First name:", self.first_name ,"\n", "Last name:", self.last_name ,"\n", "ID:", self.ID ,"\n", "Username:", User.displayUsername(self),"\n", "Email: ", User.displayEmail(self)
+
+    def displayEmail(self):
+        print self.first_name.lower() + "_" + self.last_name.lower() + "@edu.aua.am"
+
+class Student(User):
+    def __init__(self, first_name, last_name, ID, password):
+        super(Student, self).__init__(first_name, last_name, ID, password)
+        self.courses = [ ]
+
+
+    def addCourse(self, course):
+        self.courses.append(course)
+
+    def printCourses(self):
+         for k in self.courses:
+             k.getInfo()
+
+
+
+
+class Lecturer(User):
+    def __init__(self, first_name, last_name, ID, password):
+        super(Lecturer, self).__init__(first_name, last_name, ID, password)
+
+
+
+
+class Grade:
+    __min__ = 0
+    __max__ = 100
+
+    def __init__(self, grade):
+                self.__grade__ = grade
+
+    def set_grade(self, grade):
+                self.__grade__ = grade
+
+    def get_grade(self):
+                print self.__grade__
+
+    def checkIfValidGrade(self):
+            if self.__grade__ >= 0 and self.__grade__ <= 100:
+                 pass
+            else:
+                print "Please, enter a grade in range 0-100"
+
 
 class Course:
-    def __init__(self, coursename, course_ID, credits, lecturer, assignment):
+    def __init__(self, coursename, course_ID, credits, lecturer):
         self.coursename = coursename
         self.course_ID = course_ID
         self.credits = credits
         self.lecturer = lecturer
-        self.assignment = assignment
+        self.assignments = []
+
+    def addAssignments(self, name, percentage, grade, deadline, status, description):
+        assignment = Assignment(name, percentage, grade, deadline, status, description)
+        self.assignments.append(assignment)
+
+    def displayAssinments(self):
+        print "Assignments:"
+        print "-----------"
+        for k in self.assignments:
+            k.getInfo()
+            print "-------------"
 
     def getInfo(self):
-           print "Course Name:", self.coursename ,"\n", "Course ID:",self.course_ID ,"\n", "Credits:", self.credits ,"\n", "Lecturer:", self.lecturer ,"\n", "Assignment:", self.assignment
+           print "Course Name:", self.coursename ,"\n", "Course ID:",self.course_ID ,"\n", "Credits:", self.credits ,"\n", "Lecturer:", self.lecturer
+           self.displayAssinments()
 
     def checkIfValidCourseID(self):
-        if len(course1.course_ID) == 7:
+        if len(self.course_ID) == 7:
             pass
         else:
             print "Please, enter valid ID"
@@ -87,12 +114,36 @@ class Course:
         else:
             print "Please, enter valid number of credits in a range 1-4"
 
-course1 = Course ("Data Structures and Algorithms", "ENGS115", 3, "Satenik Mnatsakanyan", "Homework")
-course1.getInfo()
-course1.checkIfValidCourseID()
-course1.checkIfValidCredits()
+class AssignmentGrade:
 
+    def __init__(self, course, assignment, grade):
+        self.course = course
+        self.assignment = assignment
+        self.grade = Grade(grade)
+        self.state = 0
 
+def main():
+    
+    #assignment1.getInfo()
+    #assignment1.checkIfValidPercentage()
+    student = Student("Lina", "Gharagyozyan", "A0987645", "student.displayUsername",)
+    #student.displayUsername()
+    #student.displayInfo()
+    #grade1 = Grade(96)
+    #Grade.get_grade(grade1)
+    #Grade.checkIfValidGrade(grade1)
+    course1 = Course("Data Structures and Algorithms", "ENGS115", 3, "Satenik Mnatsakanyan",)
+    course1.addAssignments("Homework", 15, 96, "2018.15.10", "Submitted",
+                             "Find technical specification document for your laptop")
+    course1.addAssignments("Homework",15,96,"2018.16.12","Submitted","OOP Lectures")
+    #course1.getInfo()
+
+    #course1.checkIfValidCourseID()
+    #course1.checkIfValidCredits()
+    student.addCourse(course1)
+    student.printCourses()
+    
+main()
 
 
 
